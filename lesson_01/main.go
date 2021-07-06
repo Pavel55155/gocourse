3a596baaa6baa3d7fc127454232591479a756417
Pavel55155 <pavel55155@yandex.by>
2021-07-06 13:40:43 +0300

    fix after review

--- lesson_01/main.go
+++ lesson_01/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/kyokomi/emoji/v2"
 )
 
 func main() {
-	fmt.Println("Hello, world, :face:")
+	fmt.Println("Hello World Emoji!")
+
+	emoji.Println(":beer: Beer!!!")
+
+	pizzaMessage := emoji.Sprint("I like a :pizza: and :sushi:!!")
+	fmt.Println(pizzaMessage)
+	file, _ := os.Create("response.txt")
+	file.WriteString(pizzaMessage)
 }
